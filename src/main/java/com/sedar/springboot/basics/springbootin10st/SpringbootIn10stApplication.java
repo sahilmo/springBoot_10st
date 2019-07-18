c1468e9150486c3619decf09a829d56bd83ed3b3
sahil <sahil_mohammad@outlook.com>
2019-07-18 19:14:54 +0400

    Step 5 : Spring boot auto configure concept

--- src/main/java/com/sedar/springboot/basics/springbootin10st/SpringbootIn10stApplication.java
+++ src/main/java/com/sedar/springboot/basics/springbootin10st/SpringbootIn10stApplication.java
@@ -2,12 +2,16 @@ package com.sedar.springboot.basics.springbootin10st;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10stApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10stApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10stApplication.class, args);
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(" :-> " + name + "\n");
+		}
 	}
 
 }
